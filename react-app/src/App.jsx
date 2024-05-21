61df222d54f25ab15d173d5290267cbd8b4ce503
Cyrillo46 <yahooisbetter46@gmail.com>
2024-05-21 10:34:18 -0400

    divided components

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,26 +1,8 @@
 import "./App.css";
-import { Square } from "./components/Square";
+import { Board } from "./components/Board";
 
 function App() {
-  return (
-    <>
-      <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
-      </div>
-      <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
-      </div>
-      <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
-      </div>
-    </>
-  );
+  return <Board />;
 }
 
 export default App;
