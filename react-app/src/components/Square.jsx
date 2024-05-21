e44b031c8309945827f31a348e07c463684ce415
Cyrillo46 <yahooisbetter46@gmail.com>
2024-05-21 10:54:44 -0400

    removed props, added state

--- react-app/src/components/Square.jsx
+++ react-app/src/components/Square.jsx
@@ -1,3 +1,15 @@
-export const Square = ({ value }) => {
-  return <button className="square">{value}</button>;
+import { useState } from "react";
+
+export const Square = () => {
+  const [value, setValue] = useState(null);
+
+  const handleClick = () => {
+    setValue("x");
+  };
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 };
